1a18b3b4cc05cf55ca8fc7e33f290aa29beadb77
Simon Jackson <none@none>
2017-01-04 21:58:07 +0000

    Added new NonDrawingGraphic control and editor script Resolved request #107
    --HG--
    branch : develop_5.3

---
+++ Editor/NonDrawingGraphicEditor.cs
@@ -0,0 +1,21 @@
+﻿/// Credit Slipp Douglas Thompson 
+/// Sourced from - https://gist.github.com/capnslipp/349c18283f2fea316369
+/// 
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace UnityEngine.UI.Extensions
+{
+    [CanEditMultipleObjects, CustomEditor(typeof(NonDrawingGraphic), false)]
+    public class NonDrawingGraphicEditor : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.serializedObject.Update();
+            EditorGUILayout.PropertyField(base.m_Script, new GUILayoutOption[0]);
+            // skipping AppearanceControlsGUI
+            base.RaycastControlsGUI();
+            base.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Scripts/Utilities/NonDrawingGraphic.cs
@@ -0,0 +1,21 @@
+﻿/// Credit Slipp Douglas Thompson 
+/// Sourced from - https://gist.github.com/capnslipp/349c18283f2fea316369
+
+namespace UnityEngine.UI.Extensions
+{
+    /// A concrete subclass of the Unity UI `Graphic` class that just skips drawing.
+    /// Useful for providing a raycast target without actually drawing anything.
+    [AddComponentMenu("Layout/Extensions/NonDrawingGraphic")]
+    public class NonDrawingGraphic : MaskableGraphic
+    {
+        public override void SetMaterialDirty() { return; }
+        public override void SetVerticesDirty() { return; }
+
+        /// Probably not necessary since the chain of calls `Rebuild()`->`UpdateGeometry()`->`DoMeshGeneration()`->`OnPopulateMesh()` won't happen; so here really just as a fail-safe.
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+            return;
+        }
+    }
+}
