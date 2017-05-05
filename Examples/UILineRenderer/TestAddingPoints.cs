dd7d79a411f8bfcbc6c6ad3f4869510f2fb7d460
Simon Jackson <darkside@zenithmoon.com>
2017-05-05 17:14:03 +0100

    Added more example scenes

---
+++ Examples/UILineRenderer/TestAddingPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAddingPoints : MonoBehaviour {
+
+    public UnityEngine.UI.Extensions.UILineRenderer LineRenderer;
+    public UnityEngine.UI.Text XValue;
+    public UnityEngine.UI.Text YValue;
+
+    // Use this for initialization
+    public void AddNewPoint () {
+        var point = new Vector2() { x = float.Parse(XValue.text), y = float.Parse(YValue.text) };
+        var pointlist = new List<Vector2>(LineRenderer.Points);
+        pointlist.Add(point);
+        LineRenderer.Points = pointlist.ToArray();
+    }
+}
