dd0b3a3cba5cbd1ad3d906625da0b588d49f345b
Simon Jackson <darkside@zenithmoon.com>
2017-07-29 19:09:19 +0100

    Added Radial Slider and Example Still need to add Editor Option for default

---
+++ Examples/RadialSlider/UpdateRadialValue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class UpdateRadialValue : MonoBehaviour {
+
+    public InputField input;
+    public RadialSlider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void UpdateSliderValue () {
+        float value;
+        float.TryParse(input.text, out value);
+        slider.Value = value;
+	}
+
+    public void UpdateSliderAndle()
+    {
+        int value;
+        int.TryParse(input.text, out value);
+        slider.Angle = value;
+    }
+}
