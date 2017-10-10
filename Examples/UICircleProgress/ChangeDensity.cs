019d05dfb16c4c8898a34f10fbf019a43bf32438
LAB\TroyWalker <troy_s_walker@hotmail.com>
2017-10-10 16:52:02 -0500

    Major changes to UICircle with some member modifications and additions to the component behaviour.  * Added progress indicator capability to the component.  * Component uses AddUIVertexStream to generate vertices and indices of triangle order.  * Improves base geometry generation to prevent edge sliding on segment value changes (tris density, now called Arc Steps).  * Corrects a minor issue with base mesh where first vertex is generate at center (only visable in wireframe mode at certain angles).

---
+++ Examples/UICircleProgress/ChangeDensity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class ChangeDensity : MonoBehaviour
+{
+    public GameObject MultiColorObject;
+    public GameObject TextOutputObject;
+
+    private UICircle _uiCircleComponent;
+    private Text _densityOutput;
+
+    private void Awake()
+    {
+        _uiCircleComponent = MultiColorObject.GetComponent<UICircle>();
+        _densityOutput = TextOutputObject.GetComponent<Text>();
+    }
+    private void OnEnable()
+    {
+        _densityOutput.text = _uiCircleComponent.ArcSteps.ToString();
+    }
+
+    public void UpdateDensity(float value)
+    {
+        _uiCircleComponent.SetArcSteps((int)value);
+        _densityOutput.text = value.ToString();
+    }
+}
