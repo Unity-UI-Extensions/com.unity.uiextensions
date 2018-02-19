b5f8f17203adcbda4dff14018282896a58564db8
Sean Mann <naplandgames@gmail.com>
2018-02-18 23:16:15 -0500

    Improvements on Color Picker: - .gitignore - Added "/.vs" to ignore VS 2017's new folder. - Added Color Sampler feature to pick colors from the screen. - Fixed issue with ColorPickerPresets with "not working" HSVChanged() - Changed the way ColorPickerPresets work. Can now add predefined colors in the inspector, plus save/load from player prefs or json file. - Modified UI layout for Picker 2.0 prefab to allow for expansion of presets, toggling of RGB vs HSV. Still could use some work as the layout doesn't collapse/expand nicely.

--- Scripts/Controls/ColorPicker/ColorPickerControl.cs
+++ Scripts/Controls/ColorPicker/ColorPickerControl.cs
@@ -1,257 +1,303 @@
-﻿///Credit judah4
+///Credit judah4
 ///Sourced from - http://forum.unity3d.com/threads/color-picker.267043/
+using System.Collections.Generic;
 
 namespace UnityEngine.UI.Extensions.ColorPicker
 {
+	[ExecuteInEditMode]
+	public class ColorPickerControl : MonoBehaviour
+	{
+		private float _hue = 0;
+		private float _saturation = 0;
+		private float _brightness = 0;
 
-    public class ColorPickerControl : MonoBehaviour
-{
-    private float _hue = 0;
-    private float _saturation = 0;
-    private float _brightness = 0;
+		private float _red = 0;
+		private float _green = 0;
+		private float _blue = 0;
 
-    private float _red = 0;
-    private float _green = 0;
-    private float _blue = 0;
+		private float _alpha = 1;
 
-    private float _alpha = 1;
+		public ColorChangedEvent onValueChanged = new ColorChangedEvent();
+		public HSVChangedEvent onHSVChanged = new HSVChangedEvent();
 
-    public ColorChangedEvent onValueChanged = new ColorChangedEvent();
-    public HSVChangedEvent onHSVChanged = new HSVChangedEvent();
+		[SerializeField]
+		bool hsvSlidersOn = true;
 
-    public Color CurrentColor
-    {
-        get
-        {
-            return new Color(_red, _green, _blue, _alpha);
-        }
-        set
-        {
-            if (CurrentColor == value)
-                return;
+		[SerializeField]
+		List<GameObject> hsvSliders = new List<GameObject>();
 
-            _red = value.r;
-            _green = value.g;
-            _blue = value.b;
-            _alpha = value.a;
+		[SerializeField]
+		bool rgbSlidersOn = true;
 
-            RGBChanged();
-            
-            SendChangedEvent();
-        }
-    }
+		[SerializeField]
+		List<GameObject> rgbSliders = new List<GameObject>();
 
-    private void Start()
-    {
-        SendChangedEvent();
-    }
+		[SerializeField]
+		GameObject alphaSlider;
 
-    public float H
-    {
-        get
-        {
-            return _hue;
-        }
-        set
-        {
-            if (_hue == value)
-                return;
+		public void SetHSVSlidersOn(bool value)
+		{
+			hsvSlidersOn = value;
 
-            _hue = value;
+			foreach (var item in hsvSliders)
+				item.SetActive(value);
 
-            HSVChanged();
+			if (alphaSlider)
+				alphaSlider.SetActive(hsvSlidersOn || rgbSlidersOn);
+		}
 
-            SendChangedEvent();
-        }
-    }
+		public void SetRGBSlidersOn(bool value)
+		{
+			rgbSlidersOn = value;
+			foreach (var item in rgbSliders)
+				item.SetActive(value);
 
-    public float S
-    {
-        get
-        {
-            return _saturation;
-        }
-        set
-        {
-            if (_saturation == value)
-                return;
+			if (alphaSlider)
+				alphaSlider.SetActive(hsvSlidersOn || rgbSlidersOn);
+		}
 
-            _saturation = value;
 
-            HSVChanged();
+		void Update()
+		{
+#if UNITY_EDITOR
+			SetHSVSlidersOn(hsvSlidersOn);
+			SetRGBSlidersOn(rgbSlidersOn);
+#endif
+		}
 
-            SendChangedEvent();
-        }
-    }
+		public Color CurrentColor
+		{
+			get
+			{
+				return new Color(_red, _green, _blue, _alpha);
+			}
+			set
+			{
+				if (CurrentColor == value)
+					return;
 
-    public float V
-    {
-        get
-        {
-            return _brightness;
-        }
-        set
-        {
-            if (_brightness == value)
-                return;
+				_red = value.r;
+				_green = value.g;
+				_blue = value.b;
+				_alpha = value.a;
 
-            _brightness = value;
+				RGBChanged();
 
-            HSVChanged();
+				SendChangedEvent();
+			}
+		}
 
-            SendChangedEvent();
-        }
-    }
+		private void Start()
+		{
+			SendChangedEvent();
+		}
 
-    public float R
-    {
-        get
-        {
-            return _red;
-        }
-        set
-        {
-            if (_red == value)
-                return;
+		public float H
+		{
+			get
+			{
+				return _hue;
+			}
+			set
+			{
+				if (_hue == value)
+					return;
 
-            _red = value;
+				_hue = value;
 
-            RGBChanged();
+				HSVChanged();
 
-            SendChangedEvent();
-        }
-    }
+				SendChangedEvent();
+			}
+		}
 
-    public float G
-    {
-        get
-        {
-            return _green;
-        }
-        set
-        {
-            if (_green == value)
-                return;
+		public float S
+		{
+			get
+			{
+				return _saturation;
+			}
+			set
+			{
+				if (_saturation == value)
+					return;
 
-            _green = value;
+				_saturation = value;
 
-            RGBChanged();
+				HSVChanged();
 
-            SendChangedEvent();
-        }
-    }
+				SendChangedEvent();
+			}
+		}
 
-    public float B
-    {
-        get
-        {
-            return _blue;
-        }
-        set
-        {
-            if (_blue == value)
-                return;
+		public float V
+		{
+			get
+			{
+				return _brightness;
+			}
+			set
+			{
+				if (_brightness == value)
+					return;
 
-            _blue = value;
+				_brightness = value;
 
-            RGBChanged();
+				HSVChanged();
 
-            SendChangedEvent();
-        }
-    }
+				SendChangedEvent();
+			}
+		}
 
-    private float A
-    {
-        get
-        {
-            return _alpha;
-        }
-        set
-        {
-            if (_alpha == value)
-                return;
+		public float R
+		{
+			get
+			{
+				return _red;
+			}
+			set
+			{
+				if (_red == value)
+					return;
 
-            _alpha = value;
+				_red = value;
 
-            SendChangedEvent();
-        }
-    }
+				RGBChanged();
 
-    private void RGBChanged()
-    {
-        HsvColor color = HSVUtil.ConvertRgbToHsv(CurrentColor);
+				SendChangedEvent();
+			}
+		}
 
-        _hue = color.NormalizedH;
-        _saturation = color.NormalizedS;
-        _brightness = color.NormalizedV;
-    }
+		public float G
+		{
+			get
+			{
+				return _green;
+			}
+			set
+			{
+				if (_green == value)
+					return;
 
-    private void HSVChanged()
-    {
-        Color color = HSVUtil.ConvertHsvToRgb(_hue * 360, _saturation, _brightness, _alpha);
+				_green = value;
 
-        _red = color.r;
-        _green = color.g;
-        _blue = color.b;
-    }
+				RGBChanged();
 
-    private void SendChangedEvent()
-    {
-        onValueChanged.Invoke(CurrentColor);
-        onHSVChanged.Invoke(_hue, _saturation, _brightness);
-    }
+				SendChangedEvent();
+			}
+		}
 
-    public void AssignColor(ColorValues type, float value)
-    {
-        switch (type)
-        {
-            case ColorValues.R:
-                R = value;
-                break;
-            case ColorValues.G:
-                G = value;
-                break;
-            case ColorValues.B:
-                B = value;
-                break;
-            case ColorValues.A:
-                A = value;
-                break;
-            case ColorValues.Hue:
-                H = value;
-                break;
-            case ColorValues.Saturation:
-                S = value;
-                break;
-            case ColorValues.Value:
-                V = value;
-                break;
-            default:
-                break;
-        }
-    }
+		public float B
+		{
+			get
+			{
+				return _blue;
+			}
+			set
+			{
+				if (_blue == value)
+					return;
 
-    public float GetValue(ColorValues type)
-    {
-        switch (type)
-        {
-            case ColorValues.R:
-                return R;
-            case ColorValues.G:
-                return G;
-            case ColorValues.B:
-                return B;
-            case ColorValues.A:
-                return A;
-            case ColorValues.Hue:
-                return H;
-            case ColorValues.Saturation:
-                return S;
-            case ColorValues.Value:
-                return V;
-            default:
-                throw new System.NotImplementedException("");
-        }
-    }
-}
+				_blue = value;
+
+				RGBChanged();
+
+				SendChangedEvent();
+			}
+		}
+
+		private float A
+		{
+			get
+			{
+				return _alpha;
+			}
+			set
+			{
+				if (_alpha == value)
+					return;
+
+				_alpha = value;
+
+				SendChangedEvent();
+			}
+		}
+
+		private void RGBChanged()
+		{
+			HsvColor color = HSVUtil.ConvertRgbToHsv(CurrentColor);
+
+			_hue = color.NormalizedH;
+			_saturation = color.NormalizedS;
+			_brightness = color.NormalizedV;
+		}
+
+		private void HSVChanged()
+		{
+			Color color = HSVUtil.ConvertHsvToRgb(_hue * 360, _saturation, _brightness, _alpha);
+
+			_red = color.r;
+			_green = color.g;
+			_blue = color.b;
+		}
+
+		private void SendChangedEvent()
+		{
+			onValueChanged.Invoke(CurrentColor);
+			onHSVChanged.Invoke(_hue, _saturation, _brightness);
+		}
+
+		public void AssignColor(ColorValues type, float value)
+		{
+			switch (type)
+			{
+				case ColorValues.R:
+					R = value;
+					break;
+				case ColorValues.G:
+					G = value;
+					break;
+				case ColorValues.B:
+					B = value;
+					break;
+				case ColorValues.A:
+					A = value;
+					break;
+				case ColorValues.Hue:
+					H = value;
+					break;
+				case ColorValues.Saturation:
+					S = value;
+					break;
+				case ColorValues.Value:
+					V = value;
+					break;
+				default:
+					break;
+			}
+		}
+
+		public float GetValue(ColorValues type)
+		{
+			switch (type)
+			{
+				case ColorValues.R:
+					return R;
+				case ColorValues.G:
+					return G;
+				case ColorValues.B:
+					return B;
+				case ColorValues.A:
+					return A;
+				case ColorValues.Hue:
+					return H;
+				case ColorValues.Saturation:
+					return S;
+				case ColorValues.Value:
+					return V;
+				default:
+					throw new System.NotImplementedException("");
+			}
+		}
+	}
 }
