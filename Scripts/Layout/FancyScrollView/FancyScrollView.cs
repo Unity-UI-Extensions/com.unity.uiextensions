58e9772ccb718a90adc9951d0b50c4f0d28cad26
Simon Jackson <darkside@zenithmoon.com>
2019-09-27 13:30:11 +0100

    Updated Fancy Scroll view to the latest

---
+++ Scripts/Layout/FancyScrollView/FancyScrollView.cs
@@ -0,0 +1,149 @@
+﻿/// Credit setchi (https://github.com/setchi)
+/// Sourced from - https://github.com/setchi/FancyScrollView
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.UI.Extensions
+{
+    public abstract class FancyScrollView<TItemData, TContext> : MonoBehaviour where TContext : class, new()
+    {
+        [SerializeField, Range(float.Epsilon, 1f)] protected float cellInterval = 0.2f;
+        [SerializeField, Range(0f, 1f)] protected float scrollOffset = 0.5f;
+        [SerializeField] protected bool loop = false;
+        [SerializeField] protected Transform cellContainer = default;
+
+        readonly IList<FancyScrollViewCell<TItemData, TContext>> pool =
+            new List<FancyScrollViewCell<TItemData, TContext>>();
+
+        float currentPosition;
+
+        protected abstract GameObject CellPrefab { get; }
+        protected IList<TItemData> ItemsSource { get; set; } = new List<TItemData>();
+        protected TContext Context { get; } = new TContext();
+
+        /// <summary>
+        /// Updates the contents.
+        /// </summary>
+        /// <param name="itemsSource">Items source.</param>
+        protected void UpdateContents(IList<TItemData> itemsSource)
+        {
+            ItemsSource = itemsSource;
+            Refresh();
+        }
+
+        /// <summary>
+        /// Refreshes the cells.
+        /// </summary>
+        protected void Refresh() => UpdatePosition(currentPosition, true);
+
+        /// <summary>
+        /// Updates the scroll position.
+        /// </summary>
+        /// <param name="position">Position.</param>
+        protected void UpdatePosition(float position) => UpdatePosition(position, false);
+
+        void UpdatePosition(float position, bool forceRefresh)
+        {
+            currentPosition = position;
+
+            var p = position - scrollOffset / cellInterval;
+            var firstIndex = Mathf.CeilToInt(p);
+            var firstPosition = (Mathf.Ceil(p) - p) * cellInterval;
+
+            if (firstPosition + pool.Count * cellInterval < 1f)
+            {
+                ResizePool(firstPosition);
+            }
+
+            UpdateCells(firstPosition, firstIndex, forceRefresh);
+        }
+
+        void ResizePool(float firstPosition)
+        {
+            if (CellPrefab == null)
+            {
+                throw new NullReferenceException(nameof(CellPrefab));
+            }
+
+            if (cellContainer == null)
+            {
+                throw new MissingComponentException(nameof(cellContainer));
+            }
+
+            var addCount = Mathf.CeilToInt((1f - firstPosition) / cellInterval) - pool.Count;
+            for (var i = 0; i < addCount; i++)
+            {
+                var cell = Instantiate(CellPrefab, cellContainer).GetComponent<FancyScrollViewCell<TItemData, TContext>>();
+                if (cell == null)
+                {
+                    throw new MissingComponentException(
+                        $"FancyScrollViewCell<{typeof(TItemData).FullName}, {typeof(TContext).FullName}> " +
+                        $"component not found in {CellPrefab.name}.");
+                }
+
+                cell.SetupContext(Context);
+                cell.SetVisible(false);
+                pool.Add(cell);
+            }
+        }
+
+        void UpdateCells(float firstPosition, int firstIndex, bool forceRefresh)
+        {
+            for (var i = 0; i < pool.Count; i++)
+            {
+                var index = firstIndex + i;
+                var position = firstPosition + i * cellInterval;
+                var cell = pool[CircularIndex(index, pool.Count)];
+
+                if (loop)
+                {
+                    index = CircularIndex(index, ItemsSource.Count);
+                }
+
+                if (index < 0 || index >= ItemsSource.Count || position > 1f)
+                {
+                    cell.SetVisible(false);
+                    continue;
+                }
+
+                if (forceRefresh || cell.Index != index || !cell.IsVisible)
+                {
+                    cell.Index = index;
+                    cell.SetVisible(true);
+                    cell.UpdateContent(ItemsSource[index]);
+                }
+
+                cell.UpdatePosition(position);
+            }
+        }
+
+        int CircularIndex(int i, int size) => size < 1 ? 0 : i < 0 ? size - 1 + (i + 1) % size : i % size;
+
+#if UNITY_EDITOR
+        bool cachedLoop;
+        float cachedCellInterval, cachedScrollOffset;
+
+        void LateUpdate()
+        {
+            if (cachedLoop != loop || cachedCellInterval != cellInterval || cachedScrollOffset != scrollOffset)
+            {
+                cachedLoop = loop;
+                cachedCellInterval = cellInterval;
+                cachedScrollOffset = scrollOffset;
+
+                UpdatePosition(currentPosition);
+            }
+        }
+#endif
+    }
+
+    public sealed class FancyScrollViewNullContext
+    {
+    }
+
+    public abstract class FancyScrollView<TItemData> : FancyScrollView<TItemData, FancyScrollViewNullContext>
+    {
+    }
+}
--- Scripts/Layout/FancyScrollView/FancyScrollViewCell.cs
+++ Scripts/Layout/FancyScrollView/FancyScrollViewCell.cs
@@ -1,21 +1,23 @@
 ﻿/// Credit setchi (https://github.com/setchi)
 /// Sourced from - https://github.com/setchi/FancyScrollView
 
+using UnityEngine;
+
 namespace UnityEngine.UI.Extensions
 {
-    public abstract class FancyScrollViewCell<TData, TContext> : MonoBehaviour where TContext : class
+    public abstract class FancyScrollViewCell<TItemData, TContext> : MonoBehaviour where TContext : class, new()
     {
         /// <summary>
         /// Gets or sets the index of the data.
         /// </summary>
         /// <value>The index of the data.</value>
-        public int DataIndex { get; set; }
+        public int Index { get; set; } = -1;
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="T:FancyScrollView.FancyScrollViewCell`2"/> is visible.
         /// </summary>
         /// <value><c>true</c> if is visible; otherwise, <c>false</c>.</value>
-        public virtual bool IsVisible { get { return gameObject.activeSelf; } }
+        public virtual bool IsVisible => gameObject.activeSelf;
 
         /// <summary>
         /// Gets the context.
@@ -24,41 +26,32 @@ namespace UnityEngine.UI.Extensions
         protected TContext Context { get; private set; }
 
         /// <summary>
-        /// Sets the context.
+        /// Setup the context.
         /// </summary>
         /// <param name="context">Context.</param>
-        public virtual void SetContext(TContext context)
-        {
-            Context = context;
-        }
+        public virtual void SetupContext(TContext context) => Context = context;
 
         /// <summary>
         /// Sets the visible.
         /// </summary>
         /// <param name="visible">If set to <c>true</c> visible.</param>
-        public virtual void SetVisible(bool visible)
-        {
-            gameObject.SetActive(visible);
-        }
+        public virtual void SetVisible(bool visible) => gameObject.SetActive(visible);
 
         /// <summary>
         /// Updates the content.
         /// </summary>
         /// <param name="itemData">Item data.</param>
-        public virtual void UpdateContent(TData itemData)
-        {
-        }
+        public abstract void UpdateContent(TItemData itemData);
 
         /// <summary>
         /// Updates the position.
         /// </summary>
         /// <param name="position">Position.</param>
-        public virtual void UpdatePosition(float position)
-        {
-        }
+        public abstract void UpdatePosition(float position);
     }
 
-    public abstract class FancyScrollViewCell<TData> : FancyScrollViewCell<TData, FancyScrollViewNullContext>
+    public abstract class FancyScrollViewCell<TItemData> : FancyScrollViewCell<TItemData, FancyScrollViewNullContext>
     {
+        public sealed override void SetupContext(FancyScrollViewNullContext context) => base.SetupContext(context);
     }
 }
