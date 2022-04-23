771c7776c057f8e3eac1cadb8309fbdea1d06a2f
Simon Jackson <darkside@zenithmoon.com>
2022-04-23 11:45:32 +0100

    Taking in fix from https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/pull-requests/132

--- Runtime/Scripts/Primitives/UILineRenderer.cs
+++ Runtime/Scripts/Primitives/UILineRenderer.cs
@@ -466,7 +466,7 @@ namespace UnityEngine.UI.Extensions
         protected override void OnEnable()
         {
 			base.OnEnable();
-            if (m_points?.Length == 0)
+            if (m_points == null || m_points?.Length == 0)
             {
 				m_points = new Vector2[1];
             }
